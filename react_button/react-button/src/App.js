af10e415114ece5b3b9cedfdd37dce87606b9ee0
jerenteria <renteria.juan96@gmail.com>
2023-02-21 19:56:43 -0800

    not sure why its not working

--- react_button/react-button/src/App.js
+++ react_button/react-button/src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
+import React, { useState } from 'react';
+import Button from './components/Button';
 
 function App() {
-  const clickHandler = () => {
-    console.log('Changed!')
-  };
+  // let title = props.title;
+
+  // const [title, setTitle] = useState("Changed!");
+
+  // const clickHandler = () => {
+  //   setTitle("Changed!")
+  //   console.log('Changed!')
+  // };
+
+
+  // return (
+  //   <div className="button">
+  //     <button onClick={clickHandler}>Change Me!</button>
+  //   </div>
+  // );
+
+  // const sayHello = () => {
+  //   alert('You clicked me!');
+  // }
+  // // Usage
+  // return (
+  //   <button onClick={sayHello}>Default</button>
+  // );
+
+
+  const [buttonText, setButtonText] = useState("Next"); //same as creating your state variable where "Next" is the default value for buttonText and setButtonText is the setter function for your state variable instead of setState
+
+  const changeText = () => setButtonText();
 
   return (
-    <div className="button">
-      <button onClick={clickHandler}>Change Me!</button>
-    </div>
-  );
+    <Button onClick={changeText}></Button>
+  )
 }
 
 export default App;
