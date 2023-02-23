72a76d51239e4e5b636745d9de1fb70f7a4fe3c4
jerenteria <renteria.juan96@gmail.com>
2023-02-22 22:08:59 -0800

    done with small project just needed to really understand useEffect

--- react_button/react-button/src/App.js
+++ react_button/react-button/src/App.js
@@ -1,40 +1,12 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import Button from './components/Button';
 
 function App() {
-  // let title = props.title;
-
-  // const [title, setTitle] = useState("Changed!");
-
-  // const clickHandler = () => {
-  //   setTitle("Changed!")
-  //   console.log('Changed!')
-  // };
-
-
-  // return (
-  //   <div className="button">
-  //     <button onClick={clickHandler}>Change Me!</button>
-  //   </div>
-  // );
-
-  // const sayHello = () => {
-  //   alert('You clicked me!');
-  // }
-  // // Usage
-  // return (
-  //   <button onClick={sayHello}>Default</button>
-  // );
-
-
-  const [buttonText, setButtonText] = useState("Next"); //same as creating your state variable where "Next" is the default value for buttonText and setButtonText is the setter function for your state variable instead of setState
-
-  const changeText = () => setButtonText();
 
   return (
-    <Button onClick={changeText}></Button>
-  )
+    <Button/>
+  );
 }
 
 export default App;
--- react_button/react-button/src/components/Button.js
+++ react_button/react-button/src/components/Button.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './Button.module.css';
 
 const Button = () => {
+    const [title, setTitle] = useState();
+
+
+    const clickHandler = () => {
+        setTitle('You Clicked Me!')
+        console.log(title);
+    };
+
+
     return (
-        <button>Hello</button>
+        <div>
+            <p className={classes.text}>Click This Button Please!</p>
+            <button className={classes.button} onClick={clickHandler}>{title}</button>
+        </div>
     );
 };
 
